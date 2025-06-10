4f56d57af391b11430f97bb3fa978e53e90cb365
HexRabbit <h3xrabbit@gmail.com>
2025-06-11 01:30:19 +0800

    feat: add rust string prettifier

--- HappyIDA.py
+++ HappyIDA.py
@@ -5,6 +5,8 @@ import ida_hexrays
 import ida_typeinf
 import ida_kernwin
 import ida_lines
+import ida_segment
+import ida_bytes
 
 from PyQt5.QtWidgets import QApplication
 
@@ -30,6 +32,71 @@ def tag_text(text, tag):
     # address tagging doesn't have COLOR_OFF pair
     FMT = '%c%c%' + '0%dX' % ida_lines.COLOR_ADDR_SIZE + '%s'
     return FMT % (ida_lines.COLOR_ON, ida_lines.COLOR_ADDR, tag, text)
+
+class HexraysRustStringHook(ida_hexrays.Hexrays_Hooks):
+    def __init__(self):
+        super().__init__()
+        self.enable = self.detect_rust_binary()
+
+    def func_printed(self, cfunc):
+        if self.enable:
+            self.convert_rust_string(cfunc)
+        return 0
+
+    def detect_rust_binary(self):
+        ea = idc.get_name_ea_simple("rust_begin_unwind")
+        if ea != idc.BADADDR:
+            return True
+
+        segment = ida_segment.get_segm_by_name(".rodata") or \
+                        ida_segment.get_segm_by_name("__const")
+        if segment:
+            start = segment.start_ea
+            end = segment.end_ea
+
+            ea = ida_bytes.find_bytes(b'rustc-', start, end - start)
+            return ea != idc.BADADDR
+
+        return False
+
+    def convert_rust_string(self, cf):
+        ci = ida_hexrays.ctree_item_t()
+        ccode = cf.get_pseudocode()
+        for line_idx in range(cf.hdrlines, len(ccode)):
+            sl = ccode[line_idx]
+            char_idx = 0
+
+            # use a dictionary to handle cases where multiple labels reference to the same cexpr_t
+            # we only replace the variable name reference to string
+            target = {}
+            line_len = len(ida_lines.tag_remove(sl.line))
+            for char_idx in range(line_len):
+                if not cf.get_line_item(sl.line, char_idx, True, None, ci, None):
+                    continue
+
+                if not (ci.it.is_expr() and ci.e.op == ida_hexrays.cot_obj):
+                    continue
+
+                ea = ci.e.obj_ea
+                if not idc.is_strlit(ida_bytes.get_full_flags(ea)):
+                    continue
+
+                varname = ci.e.dstr()
+                if varname[0] == '"':
+                    continue
+
+                orig_string = ci.e.print1(None)
+                if orig_string in target:
+                    continue
+
+                length = ida_bytes.get_item_size(ea)
+                string = ida_bytes.get_bytes(ea, length).decode()
+                color_string = ida_lines.COLSTR(f'"{string}"', ida_lines.SCOLOR_CREF)
+                tagged_string = tag_text(color_string, ci.e.index)
+                target[orig_string] = tagged_string
+
+            for orig, mod in target.items():
+                sl.line = sl.line.replace(orig, mod)
 
 class HexraysParamLabelHook(ida_hexrays.Hexrays_Hooks):
     def func_printed(self, cfunc):
@@ -438,7 +505,8 @@ class HappyIDAPlugin(idaapi.plugin_t):
 
             # Register hexrays hooks
             self.hx_hooks = [
-                HexraysParamLabelHook()
+                HexraysParamLabelHook(),
+                HexraysRustStringHook()
             ]
             for hook in self.hx_hooks:
                 hook.hook()
